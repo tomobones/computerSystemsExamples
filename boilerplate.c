d61781e845bf449a247c1411f9e90a9893bbe369
tomobones <thomas.vogg@posteo.de>
2020-11-12 19:43:44 +0100

    simplified boilerplate.c

--- boilerplate.c
+++ boilerplate.c
@@ -1,17 +1,9 @@
+// boilerplate.c
+
 #include <stdio.h>
-
-typedef unsigned char *byte_pointer;
-
-void show_bytes(byte_pointer start, size_t len)
-{
-    int i;
-    for (i = 0; i < len; i++)
-        printf(" %.2x", start[i]);
-    printf("\n");
-}
 
 int main()
 {
-
+    printf("hello world\n");
     return 0;
 }
