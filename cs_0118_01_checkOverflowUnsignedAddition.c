6477b1f1a00f4fefda94f79865b18754f7f75c2c
tomobones <thomas.vogg@posteo.de>
2020-11-20 16:23:19 +0100

    added new code

---
+++ cs_0118_01_checkOverflowUnsignedAddition.c
@@ -0,0 +1,20 @@
+// boilerplate.c
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned i, j, s;
+    printf("Check, whether unsigned numbers will overflow\n");
+    printf("please enter first number:");
+    scanf("%u", &i);
+    printf("please enter second number:");
+    scanf("%u", &j);
+    s = i+j;
+    if (s < i || s < j)
+        printf("there will be an overflow");
+    else
+        printf("there will be no overflow");
+
+    return 0;
+}
